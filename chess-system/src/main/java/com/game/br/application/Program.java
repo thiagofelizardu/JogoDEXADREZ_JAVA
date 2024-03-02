c503bd5f469188d151aa3e6de16c7c93b97a3698
thiagofelizardo <thiagofelizardo2@gmail.com>
2024-03-02 18:11:18 -0300

    commit

---
+++ chess-system/src/main/java/com/game/br/application/Program.java
@@ -0,0 +1,11 @@
+package com.game.br.application;
+
+import com.game.br.boardgame.Board;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Board board = new Board(8,8);
+
+    }
+}
